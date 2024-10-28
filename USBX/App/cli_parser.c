73af4d88af77237ab437a7dd90427ba9dfeba90c
Marty Leisner <maleisner@gmail.com>
2024-10-28 11:55:31 -0400

    not sure if this will work

---
+++ USBX/App/cli_parser.c
@@ -0,0 +1,75 @@
+/*
+ * cli_parser.c
+ *
+ *  Created on: Oct 23, 2024
+ *      Author: leisner
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+
+struct command_line_options {
+	const char *command;
+	void (*func)(char *reply, int reply_size, int argc, char *argv[]);
+};
+
+#ifdef TEST
+
+static struct command_line_options user_options[] =
+	{
+	};
+
+static struct command_line_options developer_options[] = 
+	{
+	};
+#else
+static struct command_line_options user_options[] = 
+	{
+
+	};
+
+static struct command_line_options developer_options[] = 
+	{
+	};
+#endif
+
+
+char *parse_cli(char *input, int length)
+{
+	static char result[100];
+
+	enum { MAX_ARGS = 10 };
+
+	int argc  = 0;
+	char *argv[MAX_ARGS];
+
+	memset(result, 0, sizeof result);
+	snprintf(result, sizeof result,  "unknown option = %s\r\n", 
+			input);
+	return result;
+}
+
+
+
+
+#ifdef TEST
+main(int argc, char *argv)
+{
+	char buffer[80];
+
+	while(1) {
+		char *result;
+
+		fgets(buffer, sizeof buffer, stdin);
+		result = parse_cli(buffer, strlen(buffer));
+		printf("result = %s\n", result);
+
+	}
+
+}
+#endif
+
+
---
+++ USBX/App/cli_parser.h
@@ -0,0 +1,7 @@
+#ifndef _CLI_PARSER_H
+#define _CLI_PARSER_H
+
+char *parse_cli(char *input, int length);
+
+
+#endif
